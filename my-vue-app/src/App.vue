f41d08b9b5526cc48784d660586cb8d69bf98106
maurizio-marotta <mauriziomarotta@live.it>
2023-05-02 16:18:02 +0200

    [finish]

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,30 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+    name: "App",
+    data(){
+      return{
+        title : "La mia prima app con Vite!"
+      }
+    }
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ title }}</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+h1{
+  color: red;
 }
 </style>
